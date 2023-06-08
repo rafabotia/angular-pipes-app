75ab25b3b04f3a434e0d38bb6d1697f2f7aba6e9
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-08 17:18:00 +0200

    Modulos y Rutas de mi aplicacion

---
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-numbers-page',
+  templateUrl: './numbers-page.component.html',
+  styleUrls: ['./numbers-page.component.css']
+})
+export class NumbersPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uncommon-page',
+  templateUrl: './uncommon-page.component.html',
+  styleUrls: ['./uncommon-page.component.css']
+})
+export class UncommonPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
