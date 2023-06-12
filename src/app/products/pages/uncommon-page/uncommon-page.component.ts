b08370331787f1602bc515db3fbe150e6f58c9e0
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-12 19:33:18 +0200

    AsyncPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -41,4 +42,18 @@ export class UncommonPageComponent {
     age: 36,
     address: 'Ottawa, Canada'
   }
+
+  // Async Pipe
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap( value => console.log('tap:', value ) )
+  );
+
+  public promiseValue: Promise<string> = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      resolve( 'Tenemos data en la promesa.');
+      console.log( 'Tenemos data en la promesa.');
+      this.person.name = 'Otro nombre';
+    }, 3500);
+  })
+
 }
