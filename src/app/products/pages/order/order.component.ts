0aa9d2f60bc160b6358042b7a23f9f4ffd0df9b3
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-12 19:46:19 +0200

    Continuación de la aplicación - Pipes personalizados

---
+++ src/app/products/pages/order/order.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'products-order',
+  templateUrl: './order.component.html',
+  styles: [
+  ]
+})
+export class OrderComponent {
+
+  constructor() { }
+
+}
